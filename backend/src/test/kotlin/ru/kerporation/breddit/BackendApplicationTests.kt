6360ef2f499da872f919cac5b84665fef661b93a
kkozlovsky <kkozlovsky@yandex.ru>
2020-06-09 18:20:17 +0300

    Generate backend project

---
+++ backend/src/test/kotlin/ru/kerporation/breddit/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.kerporation.breddit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
