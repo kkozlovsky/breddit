f7b08a23709b5acebc0e05df33429f30b98f93ae
kkozlovsky <kkozlovsky@yandex.ru>
2020-06-14 19:44:58 +0300

    Fix Nullable created Date

--- backend/src/main/kotlin/ru/kerporation/breddit/dto/CommentsDto.kt
+++ backend/src/main/kotlin/ru/kerporation/breddit/dto/CommentsDto.kt
@@ -5,7 +5,8 @@ import java.time.Instant
 data class CommentsDto(
 	val id: Long?,
 	val postId: Long,
-	val createdDate: Instant,
+//	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+	val createdDate: Instant = Instant.now(),
 	val text: String,
 	val username: String
 )
